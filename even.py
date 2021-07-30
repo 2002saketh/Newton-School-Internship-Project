c03d558f1a1bd59ab46a152b53913621fdf3af72
Saketh_Coder <60027805+2002saketh@users.noreply.github.com>
2021-07-30 21:31:18 +0530

    Add files via upload
    Write a recursive solution to print even numbers from 0 to X (x will be provided as an input) (any
    language)

---
+++ even.py
@@ -0,0 +1,32 @@
+# Python3 program to implement
+# the above approach
+
+# Function to print all the
+# even numbers from 0 to X
+def Even(X):
+	
+	# Base case
+	if (X < 0):
+		return
+
+	# Recurrence relation
+	if (X % 2 == 0):
+		Even(X- 2)
+	else:
+		Even(X- 1)
+
+	# Check if X is even
+	if (X % 2 == 0):
+		print(X, end = " ")
+
+if __name__ == '__main__':
+    print("Enter starting range:") 
+    x=int(input())
+     
+    print("Even numbers:")
+ 
+    # Print all the
+    # even numbers
+    Even(x)
+    print()
+ 
